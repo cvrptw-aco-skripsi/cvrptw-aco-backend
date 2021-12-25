d23eb39fe36aca1a878037a6709ac67e07d43110
Tommy Lohil <tommylohil23052000@gmail.com>
2021-12-25 20:36:07 +0700

    Initial commit

---
+++ src/main/java/tommylohil/cvrptwaco/CvrptwAcoApplication.java
@@ -0,0 +1,13 @@
+package tommylohil.cvrptwaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvrptwAcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvrptwAcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tommylohil/cvrptwaco/CvrptwAcoApplicationTests.java
@@ -0,0 +1,13 @@
+package tommylohil.cvrptwaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvrptwAcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
